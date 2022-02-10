c9223b28376339c68d207a0b78a2f8f66348d599
ELICXIR <elicxir536329@outlook.jp>
2022-02-11 00:26:19 +0900

    2-10 update

--- Assets/ELICXIRs_BaseFramework/ObjectManager.cs
+++ Assets/ELICXIRs_BaseFramework/ObjectManager.cs
@@ -1,28 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-
+using System;
 public class ObjectManager<T> where T : ManagedObject
 {
+    //root(オブジェクトが生成される際の親オブジェクト)
+    Transform RootObject;
+
     //実際に生成されたオブジェクトのリスト
-    List<T> objects = new List<T>();
+    List<T> ObjectList = new List<T>();
 
-    //生成元のリスト
-    List<T> prefablist = new List<T>();
+    //生成元の辞書
+    Dictionary<string, T> PrefabDictionary = new Dictionary<string, T>();
 
-    T AddInstance(T prefab)
-    {
-        T newT = MonoBehaviour.Instantiate(prefab);
-        objects.Add(newT);
-        return newT;
+    public ObjectManager(Dictionary<string, T>  dictionary,GameObject root){
+
+        PrefabDictionary = new Dictionary<string, T>(dictionary);
+        RootObject = root.transform;
     }
 
+    public void AddDictionary(string key,T prefab)
+    {
+        PrefabDictionary.TryAdd(key, prefab);
+    }
+
+    public T AddInstance(T prefab)
+    {
+        if (PrefabDictionary.ContainsValue(prefab))
+        {
+            T newT = MonoBehaviour.Instantiate(prefab,RootObject);
+            ObjectList.Add(newT);
+            return newT;
+        }
+        else
+        {            
+            return null;
+        }
+    }
+    public T AddInstance(string key)
+    {
+        if (PrefabDictionary.ContainsKey(key))
+        {
+            T newT = MonoBehaviour.Instantiate(PrefabDictionary[key], RootObject);
+            ObjectList.Add(newT);
+            return newT;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public T AddInstanceAndDictionary(string key ,T prefab)
+    {        
+        AddDictionary(key, prefab);
+        return AddInstance(key);
+    }
 
 
     public virtual void Updater()
     {
-        foreach (var item in objects)
+        foreach (var item in ObjectList)
         {
             if (item.isActive())
             {
@@ -33,7 +71,7 @@ public class ObjectManager<T> where T : ManagedObject
 
     public virtual void LateUpdater()
     {
-        foreach (var item in objects)
+        foreach (var item in ObjectList)
         {
             if (item.isActive())
             {
@@ -44,7 +82,7 @@ public class ObjectManager<T> where T : ManagedObject
 
     public virtual void FixedUpdater()
     {
-        foreach (var item in objects)
+        foreach (var item in ObjectList)
         {
             if (item.isActive())
             {
