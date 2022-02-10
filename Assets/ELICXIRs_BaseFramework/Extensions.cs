4cab08a7da9079fef7ebf1e1e9c4637110fa079c
ELICXIR <elicxir536329@outlook.jp>
2022-02-10 17:49:23 +0900

    template +extension + object pool update

--- Assets/ELICXIRs_BaseFramework/Extensions.cs
+++ Assets/ELICXIRs_BaseFramework/Extensions.cs
@@ -25,7 +25,7 @@ static class VectorExtensions
     /// </summary>
     public static bool isXaxisParallel(this Vector3 v)
     {
-        return v.y == 0&& v.z==0;
+        return v.y == 0 && v.z == 0;
     }
 
     /// <summary>
@@ -33,14 +33,14 @@ static class VectorExtensions
     /// </summary>
     public static bool isYaxisParallel(this Vector3 v)
     {
-        return v.x == 0&&v.y==0;
+        return v.x == 0 && v.y == 0;
     }
     /// <summary>
     /// Z軸に平行なベクトルかどうか。
     /// </summary>
     public static bool isZaxisParallel(this Vector3 v)
     {
-        return v.x==0&&v.y == 0;
+        return v.x == 0 && v.y == 0;
     }
 
 
@@ -57,6 +57,32 @@ static class VectorExtensions
     /// </summary>
     public static bool isAxisParallel(this Vector3 v)
     {
-        return (v.isXaxisParallel() || v.isYaxisParallel()|| v.isXaxisParallel()) && v.magnitude > 0;
+        return (v.isXaxisParallel() || v.isYaxisParallel() || v.isXaxisParallel()) && v.magnitude > 0;
     }
 }
+
+///
+static class MonoBehaviourExtensions
+{
+    /// <summary>
+    /// ゲームオブジェクトがアクティブかどうか。bool値を渡すとゲームオブジェクトのアクティブ状態を変更可能
+    /// </summary>
+    public static bool isActive(this MonoBehaviour m, bool? active = null)
+    {
+        if (active == null)
+        {
+            return m.gameObject.activeSelf;
+        }
+        else
+        {
+            if(m.gameObject.activeSelf != (bool)active)
+            {
+                m.gameObject.SetActive((bool)active);
+            }
+
+            return (bool)active;
+        }
+    }
+
+}
+
--- Assets/ELICXIRs_BaseFramework/ObjectManager.cs
+++ Assets/ELICXIRs_BaseFramework/ObjectManager.cs
@@ -5,14 +5,52 @@ using UnityEngine;
 
 public class ObjectManager<T> where T : ManagedObject
 {
-
+    //実際に生成されたオブジェクトのリスト
     List<T> objects = new List<T>();
+
+    //生成元のリスト
+    List<T> prefablist = new List<T>();
 
     T AddInstance(T prefab)
     {
         T newT = MonoBehaviour.Instantiate(prefab);
+        objects.Add(newT);
         return newT;
     }
 
+
+
+    public virtual void Updater()
+    {
+        foreach (var item in objects)
+        {
+            if (item.isActive())
+            {
+                item.Updater();
+            }
+        }
+    }
+
+    public virtual void LateUpdater()
+    {
+        foreach (var item in objects)
+        {
+            if (item.isActive())
+            {
+                item.LateUpdater();
+            }
+        }
+    }
+
+    public virtual void FixedUpdater()
+    {
+        foreach (var item in objects)
+        {
+            if (item.isActive())
+            {
+                item.FixedUpdater();
+            }
+        }
+    }
 
 }
